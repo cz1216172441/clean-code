2438ed21f128252b1353ff26194cab475266f93d
roy.p.f.chen <chenro6@oocl.com>
2021-06-29 22:40:15 +0800

    refactor - extract method print lineItem

--- src/main/java/com/tw/academy/basic/$7_long_method/LineItem.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/LineItem.java
@@ -1,30 +1,34 @@
 package com.tw.academy.basic.$7_long_method;
 
 public class LineItem {
-	private String description;
-	private double price;
-	private int quantity;
+    private String description;
+    private double price;
+    private int quantity;
 
-	public LineItem(String description, double p, int quantity) {
-		super();
-		this.description = description;
-		this.price = p;
-		this.quantity = quantity;
-	}
+    public LineItem(String description, double p, int quantity) {
+        super();
+        this.description = description;
+        this.price = p;
+        this.quantity = quantity;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    public double getPrice() {
+        return price;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
     double totalAmount() {
         return price * quantity;
+    }
+
+    public String print() {
+        return String.format("%s\t%s\t%d\t%s\n", getDescription(), getPrice(), getQuantity(), totalAmount());
     }
 }
